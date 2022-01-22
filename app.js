7a43c48e61bd20ae8486e639c4039fda2dbe9540
cynthia <raspberrylounge@gmail.com>
2022-01-22 12:31:32 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const loginForm = document.querySelector("#login-form")
+const loginInput = document.querySelector("#login-form input");
+
+function onLoginSubmit(e) {
+    e.preventDefault();
+    // const username = loginInput.value;
+    console.log(e);
+}
+
+loginForm.addEventListener("submit", onLoginSubmit);
