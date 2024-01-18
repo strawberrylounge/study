7ab33a727d2d03ebfc34a24af7e693d3d0f47995
Cynthia <raspberrylounge@gmail.com>
2024-01-18 15:16:04 +0900

    fixed error

---
+++ 02-grab-market-web/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import ChildComponent, { Child } from './Child.js';
+
+function App() {
+  const text = "Cynthia";
+  const sayHello = function () {
+    return <h3>강의 너무 재밌다. test</h3>;
+  }
+  const sayHello2 = function() {
+    alert('hi');
+  }
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>{text}</h2>
+      {sayHello()}
+      <button onClick={sayHello2}>Click!</button>
+      <ChildComponent name="Cynthia" age={30} />
+      <ChildComponent name="Jason" age={26} />
+      <ChildComponent name="MinSoo" age={35 }/>
+      <ChildComponent name="YeongHee" age={24} />
+    </div>
+  );
+}
+
+export default App;
