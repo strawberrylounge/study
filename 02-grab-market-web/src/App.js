0d518a3fd92f1b63efb4404095e889d0ecc570ff
Cynthia <raspberrylounge@gmail.com>
2024-02-01 16:44:26 +0900

    (add) images, main page

--- 02-grab-market-web/src/App.js
+++ 02-grab-market-web/src/App.js
@@ -1,29 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-
-import ChildComponent, { Child } from './Child.js';
-import TimerComponent from './Timer.js';
+import MainPageComponent from './main/index.js';
 
 function App() {
-  const text = "Cynthia";
-  const sayHello = function () {
-    return <h3>강의 너무 재밌다. test</h3>;
-  }
-  const sayHello2 = function() {
-    alert('hi');
-  }
+
   return (
-    <div>
-      <h1>Hello, world!</h1>
-      <h2>{text}</h2>
-      {sayHello()}
-      <button onClick={sayHello2}>Click!</button>
-      <ChildComponent name="Cynthia" age={30} />
-      <ChildComponent name="Jason" age={26} />
-      <ChildComponent name="MinSoo" age={35 }/>
-      <ChildComponent name="YeongHee" age={24} />
-      <TimerComponent />
-    </div>
+    <MainPageComponent />
   );
 }
 
