a3c4028257a5aeaee51b41858c333afb356590cb
Cynthia <raspberrylounge@gmail.com>
2024-02-03 13:35:59 +0900

    react-router-dom 설치 및 적용

--- 02-grab-market-web/src/App.js
+++ 02-grab-market-web/src/App.js
@@ -1,10 +1,19 @@
-import './App.css';
-import MainPageComponent from './main/index.js';
+import "./App.css";
+import MainPageComponent from "./main/index.js";
+import { Route, Routes } from "react-router-dom";
+
+import UploadPage from "./upload";
+import ProductPage from "./product";
 
 function App() {
-
   return (
-    <MainPageComponent />
+    <div>
+      <Routes>
+        <Route path="/" element={<MainPageComponent />}></Route>
+        <Route path="/product" element={<ProductPage />}></Route>
+        <Route path="/upload" element={<UploadPage />}></Route>
+      </Routes>
+    </div>
   );
 }
 
